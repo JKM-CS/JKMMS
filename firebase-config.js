0bc6f72bdef4571b743ee2d5a3065dfd72f575a5
Jwan Mohammed <cuecs24004@cue.edu.krd>
2026-05-19 11:02:19 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,19 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
+    apiKey: "YOUR_API_KEY_HERE",
     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
     projectId: "YOUR_PROJECT_ID",
     storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase App
 const app = initializeApp(firebaseConfig);
-
-// Export instances to be consumed by app.js
 export const auth = getAuth(app);
 export const db = getFirestore(app);

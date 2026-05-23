7156dae4e120f9d240b2f8bbf86c26d73eaf9a64
Jwan Mohammed <cuecs24004@cue.edu.krd>
2026-05-23 18:14:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ import {
     orderBy, 
     doc, 
     updateDoc, 
-    deleteDoc,
-    onSnapshot
+    deleteDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // ==========================================

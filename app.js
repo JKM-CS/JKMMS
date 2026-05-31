0c9928bd30f1b3b3d958adc89a9bc18b8882f9f3
Jwan Mohammed <cuecs24004@cue.edu.krd>
2026-05-31 18:59:22 +0300

    Add onSnapshot import from Firebase Firestore

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ import {
     orderBy, 
     doc, 
     updateDoc, 
-    deleteDoc
+    deleteDoc,
+    onSnapshot
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // ==========================================
